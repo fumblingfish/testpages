a2ce37c1da021b93c6c849b3458137bed037d68a
Rasmus Andersson <holstandersson@gmail.com>
2016-09-14 20:35:22 +0200

    testlib import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import testlib from '../../lib/testlib'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Testing some changes</h2>
+          <h2>Testing some changes {testlib()}</h2>
         </div>
       </div>
     );
